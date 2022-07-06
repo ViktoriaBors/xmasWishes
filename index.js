fa4d8997271f95a5780ecc335503780be200efca
Viktoria Bors-Pajuste <viktoria.pajuste@gmail.com>
2022-07-06 09:19:10 +0200

    fixed input field limit

--- index.js
+++ index.js
@@ -14,12 +14,14 @@ let newItemInputField = document.getElementById("newText")
 
 function addItem(event){
     event.preventDefault();
+    if(onList < 3){
+    newItemInputField.disabled = false;
+    submit.disabled = false;
     // Add new item - text input as a value
     // must be inside the fucntion - click event
     // as a global variable - it would grab the value as a first thing - without written anything inside -gives back nothing
     let newItemInput = newItemInputField.value; 
-  
-
+    
     // create new item
     let newItem = document.createElement("li");
     //add text from input field to li 
@@ -35,50 +37,42 @@ function addItem(event){
 
     // add new item list with text and X button to UL
     itemList.appendChild(newItem)
-    onList++
-    listForm.reset() 
-    if(onList >= 3){
-        newItemInputField.disabled = true;
-        submit.disabled = true;
+    onList++ //count how many items we have on the list -  add plus one
+    newItemInputField.value = ""; // clear the input field
+    } else{
+        newItemInputField.value = ""
+        newItemInputField.placeholder = "You have reached your limit for wishes";
+        //if you want to enter 4th item and click on add item - input field clears out and placeholder reminds you have no more wishes
     }
 }
 
 // deleting items
-// dont use queryselector here - static
+//let deleteBtn = document.getElementsByClassName("delete")
+// grabs the deleteBTN which exist in that time - 0
 //after adding more items - doesnt grab these items delete button
-/*let deleteBtn = document.getElementsByClassName("delete")
-console.log(deleteBtn)
-
-for(del of deleteBtn){
-    del.addEventListener("click", function(){
-        console.log("delete")
-    })
-}
-
-still not working - just on the first button*/
+// option can be - event delegation
 
 // deleteing items by grabbing the whole ul
 let items = document.getElementById("items")
-
 
 function deleteItem(event){
     // grab just the function fires on click to delete X btn
     if(event.target.classList.contains("delete")){
         //console.log(event.target) //event target is the btn
-        //console.log("delete")
 
         // remove ul -> li
         let removeLi = event.target.parentElement;
-        //console.log(removeLi)
         items.removeChild(removeLi)
         onList--
+        newItemInputField.placeholder = "Add item";
     }
 }
 
+// Adding event listener to the button - add delete items
     submit.addEventListener("click", addItem)
     items.addEventListener("click", deleteItem)
 
-
+// adding animation to envelope and form
    let sendBtn = document.querySelector("#send")
    let envelope = document.getElementById("envelope")
    let container = document.getElementById("container")
